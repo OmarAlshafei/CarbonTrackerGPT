b0699229581815f234a644862c36f0ef6a4baa5b
omar <omaralshafei02@gmail.com>
2023-10-07 03:49:58 -0400

    Set up carbon API

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import Card from '../Components/Card.js'
 import './HomePage.css'
+// const axios = require('axios');
+// const app = express();
+
 //import suggestions from "./test.json"
 
 function HomePage() {
@@ -15,22 +18,24 @@ function HomePage() {
     setCurrMiles(event.target.value);
   }
 
-  // const updateCarbonScore = () => {
-  //   const xhr = new XMLHttpRequest();
-  //   xhr.open('GET', 'https://api.example.com/data');
-  //   xhr.onload = function() {
-  //     if (xhr.status === 200) {
-  //       setData(JSON.parse(xhr.responseText));
-  //     }
-  //   };
-  //   xhr.send();
-  //   updatedCarbonScores = [...carbonScores]
-  //   updatedCarbonScores[currDay] =  
-  //   setCarbonScores(updateCarbonScores)
-  // }
-
+  function updateCarbonScore () {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://carbonfootprint1.p.rapidapi.com/CarbonFootprintFromCarTravel?distance=100&vehicle=SmallDieselCar');
+    xhr.setRequestHeader("X-RapidAPI-Key", "c8978ec990msh123912b707b02b7p159836jsn73eac02d58f3"); 
+    xhr.setRequestHeader("X-RapidAPI-Host", "carbonfootprint1.p.rapidapi.com");
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        console.log(JSON.parse(xhr.responseText));
+      }
+    };
+    xhr.send();
+  }
+  
   return (
-    <div>
+    <div className="homePage">
+      <div class = 'appTitle'>
+        <h1>Carbon Tracker GPT</h1>
+      </div>
       <div className="calendar">
         <Card day={dayMap[0]} carbonScore={carbonScores[0]}></Card>
         <Card day={dayMap[1]} carbonScore={carbonScores[1]}></Card>
@@ -44,7 +49,7 @@ function HomePage() {
       <div className="milesContainer">
         <input onChange={handleChange}></input>
         <h3>Miles</h3>
-        {/* <button className="milesSubmit" onClick={updateCarbonScore}></button> */}
+        <button type="submit" onClick={updateCarbonScore}>Submit</button>
       </div>
       <div className="carbonScoreContainer">
         <h3>Carbon Score: {carbonScores[currDay]}</h3>
