bf4abfd717f750407af8c26566745e3dd506e1a4
nstuhldreher1 <nbstuhldreher@gmail.com>
2023-10-07 15:45:22 -0400

    Added server.js

--- frontend/src/Components/ChatGPT.js
+++ frontend/src/Components/ChatGPT.js
@@ -1,9 +1,34 @@
 import './ChatGPT.css';
+import React, {useEffect, useState} from 'react';
 
-function ChatGPT(){
+function ChatGPT(prompt){
+    
+    const [responseGPT, setResponseGPT] = useState([]);
+    
+    useEffect(() => {
+        promptGPT();
+    }, []);
+   
+    const promptGPT = async () =>{
+        const response = await fetch('http://localhost:8000/api/chatGPT',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                prompt: prompt
+            }),
+        });
+
+        const data = await response.json();
+        console.log(data);
+        setResponseGPT(data.message);
+    }   
+
     return(
         <div id="ChatGPT">
             <h1 id="chatGPTTitle">Here's how you can help.</h1>
+            <p>{responseGPT}</p>
         </div>
     );
 }
--- server.js
+++ server.js
@@ -1,94 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const axios = require('axios');
+
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const MongoClient = require('mongodb').MongoClient;
+const OpenAI = require('openai');
 
-const url = 'mongodb+srv://oa:oa@cluster0.fyrwd6g.mongodb.net/?retryWrites=true&w=majority';
-const client = new MongoClient(url);
-client.connect();
-
+require('dotenv').config();
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
-    next();
+app.post("/api/chatGPT", async (req, res) => {
+  let {prompt} = req.body;
+
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+  });
+
+  const chatCompletion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [{"role": "user", "content": "Hello!"}],
+  });
+
+  res.json({ message: chatCompletion });
 });
 
-app.listen(5000);
-
-app.post('/api/register', async (req, res, next) => {
-    const { login, password, firstName, lastName } = req.body;
-    const db = client.db('CarbonTrackerGPT');
-    const error = '';
-
-    try {
-        const existingUser = await db.collection('Users').findOne({ Login: login });
-        if (existingUser) {
-            error = 'User with this login already exists';
-        } else {
-            const result = await db.collection('Users').insertOne({
-                Login: login,
-                Password: password,
-                FirstName: firstName,
-                LastName: lastName
-            });
-            if (result.insertedId) {
-                res.status(200).json({ id: result.insertedId, error: '' });
-                return;
-            } else {
-                error = 'User registration failed';
-            }
-        }
-    } catch (e) {
-        error = e.toString();
-    }
-    res.status(200).json({ id: -1, error: error });
-});
-
-app.post('/api/login', async (req, res, next) => {
-    const error = '';
-    const { login, password } = req.body;
-    const db = client.db('CarbonTrackerGPT');
-    const results = await db.collection('Users').find({ Login: login, Password: password }).toArray();
-    let id = -1;
-    let fn = '';
-    let ln = '';
-    if (results.length > 0) {
-        id = results[0].UserId;
-        fn = results[0].FirstName;
-        ln = results[0].LastName;
-    }
-    const ret = { id: id, firstName: fn, lastName: ln, error: '' };
-    res.status(200).json(ret);
-});
-
-app.post('/api/trackCarbon', async (req, res, next) => {
-    const { distance, vehicle } = req.body;
-
-    const options = {
-        method: 'GET',
-        url: 'https://carbonfootprint1.p.rapidapi.com/CarbonFootprintFromCarTravel',
-        params: {
-            distance: distance || '',
-            vehicle: vehicle || ''
-        },
-        headers: {
-            'X-RapidAPI-Key': 'b4306ba6e6msh94925cf0c0d0330p1d7fa4jsn16de956e44ea',
-            'X-RapidAPI-Host': 'carbonfootprint1.p.rapidapi.com'
-        }
-    };
-
-    try {
-        const response = await axios.request(options);
-        console.log(response.data);
-        res.status(200).json(response.data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'API request failed' });
-    }
+app.listen(8000, () => {
+  console.log('Server is running on port 8000.');
 });
