0efd126b4aab78aaf701234d9bbc011f2f653cbb
omar <omaralshafei02@gmail.com>
2023-10-07 20:56:24 -0400

    almost there

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -7,18 +7,20 @@ import axios from 'axios';
 
 function HomePage() {
     const dayMap = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const [carbonScores, setCarbonScores] = useState([0, 0, 0, 0, 0, 0, 0]);
     const [miles, setMiles] = useState([0, 0, 0, 0, 0, 0, 0]);
     const [currDay, setCurrDay] = useState(0);
-    const [currMiles, setCurrMiles] = useState(0);
-    const [currVehicle, setCurrVehicle] = useState('SmallPetrolCar');
+    const [currMiles, setCurrMiles] = useState([]);
     const [totalCarbonScore, setTotalCarbonScore] = useState(0);
     const [currMake, setCurrMake] = useState('Ford');
     const [currModel, setCurrModel] = useState('Focus');
     const [currUnit, setCurrUnit] = useState('mi');
-    const handleChange = (event) => {
-    setCurrMiles(event.target.value);
+    const baseURL = "https://jsonplaceholder.typicode.com/posts";
+  
+  
+  const handleChange = (event) => {
+    setCurrMiles(parseInt(event.target.value));
   };
+
 
   const setMilesForDay = () => {
     const updatedMiles = [...miles];
@@ -26,19 +28,45 @@ function HomePage() {
     setMiles(updatedMiles);
     setCurrMiles(0);
   };
-  
-  const updateCarbonScore = () => {
-    const updatedCarbonScores = [...carbonScores];
-    const totalMiles = d3.sum(miles);
+
+  async function carbonScore() {
+    console.log(currMake);
+    console.log(currModel);
+    console.log(miles);
     
-// api shit here
+    let totalMiles = 0;
     
-    updatedCarbonScores[currDay] = totalMiles;
-    setCarbonScores(updatedCarbonScores);
+    //Add all of the miles
+    for(let i = 0; i < 7; i++){
+     totalMiles += miles[i];
+    }
+
+    console.log(totalMiles);
+    const encodedParams = new URLSearchParams();
+    encodedParams.set('vehicle_make', currMake);
+    encodedParams.set('vehicle_model', currModel);
+    encodedParams.set('distance_value', totalMiles);
+    encodedParams.set('distance_unit', currUnit);
     
-    setTotalCarbonScore(3);
-    // xhr.send();
-  };
+    const options = {
+      method: 'POST',
+      url: 'https://carbonsutra1.p.rapidapi.com/vehicle_estimate_by_model',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        Authorization: 'Bearer fQ98oU704xFvsnXcQLVDbpeCJHPglG1DcxiMLKfpeNEMGumlbzVf1lCI6ZBx',
+        'X-RapidAPI-Key': 'c8978ec990msh123912b707b02b7p159836jsn73eac02d58f3',
+        'X-RapidAPI-Host': 'carbonsutra1.p.rapidapi.com'
+      },
+      data: encodedParams,
+    };
+    
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
   
   return (
     <div id = "HomePage">
@@ -56,7 +84,7 @@ function HomePage() {
         <div className="infoContainer">
           <div className="milesHandler">
           <input className='milesInput' type="number" value={currMiles} onChange={handleChange} />
-            <select className='unitSelect' value={currUnit} onChange={(e) => setCurrUnit(e.target.value)}>
+            <select className='unitSelect' value={currUnit} onChange={(e) => (e.target.value)}>
               <option value="mi">mi</option>
               <option value="km">km</option>
             </select>
@@ -64,11 +92,11 @@ function HomePage() {
           </div>
           <div className="vehicleHandler">
             <h3>Enter Make and Model</h3>
-            <input className='makeInput' type="text" value={currMake} placeholder="Make" />
-            <input className='modelInput' type="text" value={currModel} placeholder="Model" />
+            <input className='makeInput' type="text" value={currMake} placeholder="Make" onChange={(e) => setCurrMake(e.target.value)} />
+            <input className='modelInput' type="text" value={currModel} placeholder="Model" onChange={(e) => setCurrModel(e.target.value)} />
           </div>
         </div>
-        <button id="submitButton" className="submit" onClick={updateCarbonScore}>Submit</button>
+        <button id="submitButton" className="submit" onClick={carbonScore}>Submit</button>
         <div className="carbonScoreContainer">
           <h3>Carbon Score: {totalCarbonScore.toFixed(2)}</h3>
         </div>
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const OpenAI = require('openai');
+const axios = require('axios');
 
 require('dotenv').config();
 app.use(cors());
@@ -25,6 +26,39 @@ app.post("/api/chatGPT", async (req, res) => {
   res.json({ message: chatCompletion.choices[0].message });
 });
 
+
+app.post("/api/CarbonEmissions", async (req, res)=>{
+  let {make, model, miles} = req.body;
+
+  const encodedParams = new URLSearchParams();
+  encodedParams.set('vehicle_make', make);
+  encodedParams.set('vehicle_model', model);
+  encodedParams.set('distance_value', miles);
+  encodedParams.set('distance_unit', 'mi');
+
+  const options = {
+    method: 'POST',
+    url: 'https://carbonsutra1.p.rapidapi.com/vehicle_estimate_by_model',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: 'Bearer fQ98oU704xFvsnXcQLVDbpeCJHPglG1DcxiMLKfpeNEMGumlbzVf1lCI6ZBx',
+      'X-RapidAPI-Key': 'c8978ec990msh123912b707b02b7p159836jsn73eac02d58f3',
+      'X-RapidAPI-Host': 'carbonsutra1.p.rapidapi.com'
+    },
+    data: encodedParams,
+  };
+
+  try {
+    const response = await axios.request(options);
+    console.log(response.data);
+    res.json({response: response}).
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+
 app.listen(8000, () => {
   console.log('Server is running on port 8000.');
 });
+
